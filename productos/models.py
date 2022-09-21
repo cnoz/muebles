aa311f651532aa37e8d83753062b45277836ea17
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2022-09-21 20:20:28 -0300

    desarrollo final

--- productos/models.py
+++ productos/models.py
@@ -9,12 +9,20 @@ class Mesa(models.Model):
     tipo= models.CharField(max_length= 30)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return f'nombre: {self.nombre} - material: {self.material} - tipo: {self.tipo} - precio: {self.precio}'
+
 
 class Silla(models.Model):
     nombre = models.CharField(max_length= 30)
     material = models.CharField(max_length= 30)
     tipo = models.CharField(max_length= 30)
     precio = models.IntegerField()
+
+    
+    def __str__(self):
+        return f'nombre: {self.nombre} - material: {self.material} - tipo: {self.tipo} - precio: {self.precio}'
+
 
 
 class Sofa(models.Model):
@@ -23,9 +31,17 @@ class Sofa(models.Model):
     tipo = models.CharField(max_length= 30)
     precio = models.IntegerField()
 
+    
+    def __str__(self):
+        return f'nombre: {self.nombre} - material: {self.material} - tipo: {self.tipo} - precio: {self.precio}'
+
+
 class Usuario(models.Model):
     nombre = models.CharField(max_length= 30)
     apellido= models.CharField(max_length= 30)
     email = models.EmailField()
     telefono = models.IntegerField()
     
+    
+    def __str__(self):
+        return f'nombre: {self.nombre} - apellido: {self.apellido} - email: {self.email} - telefono: {self.telefono}'
