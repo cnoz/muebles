742a89fba9f8346c5813df314b636ee9f3cbfcc4
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2022-09-21 21:22:19 -0300

    prefinal entrega

--- muebles/urls.py
+++ muebles/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from muebles.view import *
+from productos.views import  inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', inicio),
     path('productos/', include ('productos.urls'))
 ]
