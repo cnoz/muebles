400c1a5cd90bcb3af8ffc06bfe156e621942a093
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2022-10-07 15:03:30 -0300

    cambio el inicio en urls.py muebles para que al ingresar a la pagina te pida iniciar sesio o registrarte. mantiene el mensaje de bienvenida.

--- muebles/urls.py
+++ muebles/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
+    path('', home),
     path('productos/', include ('productos.urls'))
 ]
 
