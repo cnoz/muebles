afad6147cf137ae272228805e6683fb6ae16a251
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2022-10-15 21:07:12 -0300

    pequeños cambios esteticos en el admin

--- productos/admin.py
+++ productos/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Mesa)
+class MesaAdmin(admin.ModelAdmin):
+    list_display=('nombre', 'material', 'tipo', 'precio')
+    search_fields=('nombre', 'precio','tipo')
+    list_filter=('material',)
+    
+admin.site.register(Mesa, MesaAdmin)
 admin.site.register(Silla)
 admin.site.register(Sofa)
 admin.site.register(Usuario)
